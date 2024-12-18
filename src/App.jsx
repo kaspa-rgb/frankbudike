0c2b399f0d0f3f720e93662640942cb2dfc9d789
Supreme Guru <150984872+SupremeGuru@users.noreply.github.com>
2024-12-18 00:48:12 +0000

    the final edit for the year

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,26 @@ import Footer from './components/Footer';
 import Drawing from './pages/Drawing';
 import Biography from './pages/Biography';
 import ErrorPage from './pages/404Page';
+import Hero from './components/Hero'
+import Gallery from './components/Gallery'
+import BiographyCard from './components/BiographyCard'
+import Services from './components/Services'
 
 const App = () => {
   return (
-    <div className='bg-black'>
+    <div className='bg-black' id='home'>
       <Navbar/>
       <Routes>
-        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/' element={
+              <>
+                <Hero/>
+                <Gallery/>
+                <div className='p-2 mt-1'>
+                <BiographyCard/>
+                </div>
+                <Services/>
+              </>
+        } />
         <Route exact path='/drawing' element={<Drawings/>} />
         <Route exact path='/drawing/:artId' element={<Drawing/>} />
         <Route exact path='/biography' element={<Biography/>} />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-        <div className="hero relative w-full h-0">
+        <div className="hero w-full">
             <div className="flex flex-col justify-end absolute bottom-0 w-full h-0 text-center pb-[80px] sm:pb-[26px]">
                 <div className='ml-auto'>
-                <h1 className="text-[90px] sm:text-[120px] md:text-[140px] lg:text-[172px] font-playFair font-semibold leading-none">
-                <span className="block just-sserif text-black pr-[140px] sm:pr-[230px] md:pr-[280px] lg:pr-[500px] sm:-mt-[20px] hide_tap"> <span className='text-gray-500 just-sserif'>Hi</span> w're</span>
-                    <span className="block just-sserif text-black -ml-[80px] sm:-ml-[160px] md:-ml-[200px] lg:-ml-[240px] sm:-mt-[20px] hide_tap md:-mt-[20px] lg:-mt-[20px]">Frank</span>
-                    <span className="block just-sserif text-gray-500 pl-[80px] sm:pl-[130px] md:pl-[130px] lg:pl-[170px] sm:-mt-[40px] hide_tap">Studios</span>
+                <h1 className="text-[90px] sm:text-[100px] md:text-[140px] lg:text-[110px] font-playFair font-semibold leading-none">
+                <span className="block just-sserif text-black pr-[140px] sm:pr-[230px] md:pr-[280px] lg:pr-[500px] sm:-mt-[20px] hide_tap"> <span className='text-white-500 just-sserif'>Hi</span> w're</span>
+                    <span className="block just-sserif text-white -ml-[80px] sm:-ml-[160px] md:-ml-[200px] lg:-ml-[240px] sm:-mt-[20px] hide_tap md:-mt-[20px] lg:-mt-[20px]">Frank</span>
+                    <span className="block just-sserif text-white-500 pl-[80px] sm:pl-[130px] md:pl-[130px] lg:pl-[170px] sm:-mt-[40px] lg:mt-[2px] hide_tap">Studios</span>
                 </h1>
                 </div>
                 <a href="#gallery" className="flex justify-center mt-[30px] sm:mt-[50px] w-[50px] mx-auto">
